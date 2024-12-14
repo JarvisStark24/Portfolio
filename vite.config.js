e1eac42e4afcf7015370c5ca98c3be471398a844
JarvisStark24 <dass96852@gmail.com>
2024-12-14 18:08:40 +0530

    server changes made again

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 80, // Ensure the server binds to the correct port (either the PORT environment variable or 80)
-  },
 })
